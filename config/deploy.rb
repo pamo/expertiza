ec4efb8d44379c998fd3ce82d677e72eeeeeb091
Pamela Ocampo <pamela.ocampo@gmail.com>
2013-04-23 20:41:58 -0400

    fetch latest gitiginore

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,13 @@
 require 'bundler/capistrano'
 
+task :alldos2unix do
+  `find ./*`.split("\n").each do |str|
+    `dos2unix #{str}`
+  end
+end
+
 set :application, "expertiza"
 set :repository,  "git://github.com/expertiza/expertiza.git"
-set :user, "rails"
 set :use_sudo, false
 
 set :scm, :git
@@ -12,17 +17,49 @@ set :bundle_without,  [:development, :test]
 
 set :deploy_to, "/local/rails/expertiza"
 set :runner, "www-data"
-set :branch do
-  default_branch = 'production'
-  branch = Capistrano::CLI.ui.ask "Branch to deploy (make sure to push first) [#{default_branch}]: "
-  branch = default_branch if branch.empty?
-  branch
+
+desc "Run tasks in staging enviroment."
+task :staging do
+  puts "*** Deploying to the \033[1;42m  STAGING  \033[0m server!"
+  role :web, "test.expertiza.csc.ncsu.edu"
+  role :app, "test.expertiza.csc.ncsu.edu"
+  role :cron, "test.expertiza.csc.ncsu.edu"
+  role :db,  "test.expertiza.csc.ncsu.edu", :primary => true # This is where Rails migrations will run
+  
+  set :user do
+    user = Capistrano::CLI.ui.ask "User to log in as: "
+  end
+  
+  set :branch do
+    default_branch = 'staging'
+    
+    branch = Capistrano::CLI.ui.ask "Branch to deploy (make sure to push first) [#{default_branch}]: "
+    branch = default_branch if branch.empty?
+    branch
+  end
 end
 
-role :web, "expertiza.ncsu.edu"
-role :app, "expertiza.ncsu.edu"
-role :cron, "expertiza.ncsu.edu"
-role :db,  "expertiza.ncsu.edu", :primary => true # This is where Rails migrations will run
+desc "Run tasks in staging enviroment."
+task :production do
+  puts "*** Deploying to the \033[1;41m  PRODUCTION  \033[0m servers!"
+  role :web, "expertiza.ncsu.edu"
+  role :app, "expertiza.ncsu.edu"
+  role :cron, "expertiza.ncsu.edu"
+  role :db,  "expertiza.ncsu.edu", :primary => true # This is where Rails migrations will run
+  
+  set :user do
+    user = Capistrano::CLI.ui.ask "User to log in as: "
+  end
+  
+  set :branch do
+    default_branch = 'production'
+    
+    branch = Capistrano::CLI.ui.ask "Branch to deploy (make sure to push first) [#{default_branch}]: "
+    branch = default_branch if branch.empty?
+    branch
+  end
+end
+
 
 namespace :deploy do
   task :stop do; end
@@ -35,22 +72,23 @@ namespace :deploy do
 
   desc "Symlink shared files into the current deploy directory."
   task :symlink_shared do
-    run "ln -s #{shared_path}/pg_data #{current_path}"
-    run "ln -sf #{shared_path}/database.yml #{current_path}/config/database.yml"
+    run "ln -s #{shared_path}/pg_data #{release_path}"
+    run "ln -sf #{shared_path}/database.yml #{release_path}/config/database.yml"
   end
 end
 
-after "deploy:symlink", "deploy:symlink_shared"
+after "deploy:update_code", "deploy:symlink_shared"
 
-desc "Load production data into the local development database."
-task :load_production_data, :roles => :db, :only => { :primary => true } do
+desc "Load data into the local development database."
+task :load_data, :roles => :db, :only => { :primary => true } do
   require 'yaml'
  
   database = YAML::load_file('config/database.yml')
   filename = "dump.#{Time.now.strftime '%Y-%m-%d_%H:%M:%S'}.sql.gz"
+  command = "mysqldump -u #{database['production']['username']} --password=#{database['production']['password']} #{database['production']['database']} --add-drop-table | gzip > /tmp/#{filename}"
  
   on_rollback { delete "/tmp/#{filename}" }
-  run "mysqldump -u #{database['production']['username']} --password=#{database['production']['password']} #{database['production']['database']} --add-drop-table | gzip > /tmp/#{filename}" do |channel, stream, data|
+  run command do |channel, stream, data|
     puts data
   end
 
@@ -63,3 +101,6 @@ task :load_production_data, :roles => :db, :only => { :primary => true } do
   logger.info 'Importing production database into local development database'
   system "gunzip -c #{filename} | mysql -u #{database['development']['username']} --password=#{database['development']['password']} #{database['development']['database']} && rm -f #{filename}"
 end
+
+set :default_environment, 'JAVA_HOME' => "/etc/alternatives/java_sdk/"
+# set :default_environment, 'JAVA_HOME' => "/usr/lib/jvm/java-6-openjdk/"
